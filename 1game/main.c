29812b4d22fbb62b0dcd30fdedf81d0b31e1a5b1
BQOR <d5k7qnzszn@privaterelay.appleid.com>
2026-05-12 15:50:55 +0900

    .

--- 1game/main.c
+++ 1game/main.c
@@ -1,4 +1,396 @@
 #include <stdio.h>
-#include <Windows.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
 
-tlqkffus
+#define COLOR_RESET "\x1b[0m"
+
+#define FONT_COLOR_BLACK 30
+#define BG_COLOR_BLACK 40
+#define FONT_COLOR_RED 31
+#define BG_COLOR_RED 41
+#define FONT_COLOR_GREEN 32
+#define BG_COLOR_GREEN 42
+#define FONT_COLOR_YELLOW 33
+#define BG_COLOR_YELLOW 43
+#define FONT_COLOR_BLUE 34
+#define BG_COLOR_BLUE 44
+#define FONT_COLOR_MAGENTA 35
+#define BG_COLOR_MAGENTA 45
+#define FONT_COLOR_BRIGHTMAGENTA 95
+#define BG_COLOR_BRIGHTMAGENTA 105
+#define FONT_COLOR_WHITE 37
+#define BG_COLOR_WHITE 47
+
+#define Backspace 8
+
+void set_color(int code);
+int move_cursor(int x, int y);
+void ShowLogo(void);
+int RenderTitle(void);
+int People(void);
+int Manual(void);
+int Gamestart(void);
+int Gameover(void);
+
+int menu = 1;
+int isRunning = 1;
+
+void ShowLogo(void)
+{
+    printf("\x1b[2J");
+
+    int box_width = 60;
+    int box_height = 24;
+    int box_start_x = 37;
+    int box_start_y = 5;
+
+    for (int i = 0; i < box_height; i++) {
+        printf("\x1b[%d;%dH", box_start_y + i, box_start_x);
+        printf("\x1b[44m");
+        for (int j = 0; j < box_width; j++) {
+            printf(" ");
+        }
+        printf("\x1b[0m");
+    }
+
+    const char* logo[] = {
+       "                                                   ",
+       "  WWWWWW                                           ",
+       " WWW   WWWBB                                       ",
+       " WWW   WWWBB                                       ",
+       " WWW   WWWBB   XXXXXX  XXXXX  XXXXX                 ",
+       "  WWWWWWWBB        X   X   X  X                     ",
+       "    BBBBB         X    XXXXX  XXXXX                 ",
+       "                 X     X   X  X                     ",
+       "               XXXXXX  X   X  XXXXX                 ",
+       "                                                    ",
+       "                 XXX   X   X  XXXXX                 ",
+       "                X   X  XX  X  X                     ",
+       "                X   X  X X X  XXXXX                 ",
+       "                X   X  X  XX  X                     ",
+       "                 XXX   X   X  XXXXX      WWW        ",
+       "                                       WWWWWBB       ",
+       "                                       WW WWBB       ",
+       "                                          WWBB       ",
+       "                                          WWBB       ",
+       "                                      WWWWWWWWBB     ",
+       "                                        BBBBBB       "
+    };
+
+    int logo_height = 21;
+    int logo_width = 52;
+
+    int logo_start_x = box_start_x + (box_width - logo_width) / 2;
+    int logo_start_y = box_start_y + (box_height - logo_height) / 2;
+
+    for (int i = 0; i < logo_height; i++) {
+        printf("\x1b[%d;%dH", logo_start_y + i, logo_start_x);
+        for (int j = 0; j < logo_width; j++) {
+            if (logo[i][j] == 'W') {
+                printf("\x1b[47m ");
+            }
+            else if (logo[i][j] == 'B' || logo[i][j] == 'X') {
+                printf("\x1b[40m ");
+            }
+            else {
+                printf("\x1b[44m ");
+            }
+        }
+        printf("\x1b[0m");
+    }
+
+    printf("\x1b[%d;1H\n", box_start_y + box_height + 1);
+
+    Sleep(2000);
+    system("cls");
+
+    return 0;
+}
+
+int RenderTitle(void)
+{
+    set_color(BG_COLOR_BRIGHTMAGENTA);
+    set_color(FONT_COLOR_WHITE);
+
+    move_cursor(52, 9);
+    printf("                       ");
+    move_cursor(52, 10);
+    printf("       이걸 죽네       ");
+    move_cursor(52, 11);
+    printf("                       ");
+
+    set_color(BG_COLOR_BLACK);
+
+    if (menu == 1)
+    {
+        set_color(BG_COLOR_YELLOW);
+    }
+    move_cursor(52, 13);
+    printf("  1. 만든 사람 및 팀 소개  ");
+    set_color(BG_COLOR_BLACK);
+
+    if (menu == 2)
+    {
+        set_color(BG_COLOR_YELLOW);
+    }
+    move_cursor(52, 15);
+    printf("  2. 설명서  ");
+    set_color(BG_COLOR_BLACK);
+
+    if (menu == 3)
+    {
+        set_color(BG_COLOR_YELLOW);
+    }
+    move_cursor(52, 17);
+    printf("  3. 게임 시작  ");
+    set_color(BG_COLOR_BLACK);
+
+    if (menu == 4)
+    {
+        set_color(BG_COLOR_YELLOW);
+    }
+    move_cursor(52, 19);
+    printf("  4. 게임 종료  ");
+    set_color(BG_COLOR_BLACK);
+
+    char a = getch();
+
+    switch (a)
+    {
+    case 'w':
+        if (menu > 1)
+        {
+            menu = menu - 1;
+        }
+        break;
+
+    case 's':
+        if (menu < 4)
+        {
+            menu = menu + 1;
+        }
+        break;
+
+    case 27:
+        isRunning = 0;
+        break;
+
+    case 13:
+        if (menu == 1)
+        {
+            return 1;
+        }
+        if (menu == 2)
+        {
+            return 2;
+        }
+        if (menu == 3)
+        {
+            return 3;
+        }
+
+        if (menu == 4)
+        {
+            return 4;
+        }
+        break;
+    }
+
+    return 0;
+}
+
+
+int People(void)
+{
+
+    system("cls");
+
+    printf("\x1b[2J");
+
+    int box_width = 50;
+    int box_height = 30;
+
+    int box_start_x = 40;
+    int box_start_y = 5;
+
+    for (int i = 0; i < box_height; i++) {
+        printf("\x1b[%d;%dH", box_start_y + i, box_start_x);
+
+        printf("\x1b[44m");
+        for (int j = 0; j < box_width; j++) {
+            printf(" ");
+        }
+        printf("\x1b[0m");
+    }
+
+    const char* title_text[] = {
+       "XXXXX  XXXXX  XXXX   XXXXX  X   X  XXXXX",
+       "   X   X      X   X  X   X  XX  X  X    ",
+       "  X    XXXX   XXXX   X   X  X X X  XXXX ",
+       " X     X      X  X   X   X  X  XX  X    ",
+       "XXXXX  XXXXX  X   X  XXXXX  X   X  XXXXX"
+    };
+
+
+    int title_width = (int)strlen(title_text[0]);
+    int title_start_x = box_start_x + (box_width - title_width) / 2;
+    int title_start_y = box_start_y + (box_height - 11) / 2;
+
+    for (int i = 0; i < 5; i++) {
+        printf("\x1b[%d;%dH", title_start_y + i, title_start_x);
+        for (int j = 0; j < title_width; j++) {
+            if (title_text[i][j] == 'X') {
+                printf("\x1b[40m ");
+            }
+            else {
+                printf("\x1b[44m ");
+            }
+        }
+        printf("\x1b[0m");
+    }
+    const char* team_text[] = {
+       " team 01 (ZERONE) 팀원들",
+       "마준서(202617166) : 총괄",
+       "백종화(202617139) : 코드",
+       "이인욱(202619389) : 코드",
+       "이준현(202619549) : 디자인"
+    };
+
+    int team_start_x = box_start_x + (box_width - 24) / 2;
+    int team_start_y = title_start_y + 5 + 1;
+
+    for (int i = 0; i < 5; i++) {
+        printf("\x1b[%d;%dH\x1b[30m\x1b[44m%s\x1b[0m", team_start_y + i, team_start_x, team_text[i]);
+    }
+
+    printf("\x1b[%d;1H\n", box_start_y + box_height + 1);
+
+    move_cursor(48, 30);
+    printf("Backspace를 눌러 메뉴로 돌아가시오");
+
+    int key = 0;
+
+    while (key != 8)
+    {
+        key = _getch();
+
+        if (key == 27)
+        {
+            exit(0);
+        }
+    }
+
+    system("cls");
+
+    return 0;
+}
+
+int Manual(void)
+{
+    system("cls");
+
+    move_cursor(52, 13);
+    printf("여기에 게임 설명 적기. 일단은 Backspace를 눌러서 메뉴로 가기지만 키 지정하기");
+
+    int key = 0;
+
+    while (key != 8)
+    {
+        key = _getch();
+
+        if (key == 27)
+        {
+            exit(0);
+        }
+    }
+
+    system("cls");
+
+    return 0;
+}
+
+int Gamestart(void)
+{
+    system("cls");
+
+    move_cursor(52, 13);
+    printf("여기는 게임 시작. 메뉴로 돌아갈때 Backspace 누르기. 일단은 Backspace 누르고 나중에 키 지정하고 게임 여기에 만들기");
+
+    int key = 0;
+
+    while (key != 8)
+    {
+        key = _getch();
+
+        if (key == 27)
+        {
+            exit(0);
+        }
+    }
+
+    system("cls");
+
+    return 0;
+}
+
+int Gameover(void)
+{
+    system("cls");
+
+    move_cursor(52, 13);
+    printf("여기에 마무리 되는거 추가로 넣고 꺼지게 하기");
+
+    Sleep(2000);
+
+    exit(0);
+}
+
+
+
+void set_color(int code)
+{
+    printf("\x1b[%dm", code);
+}
+
+int move_cursor(int x, int y)
+{
+    printf("\033[%d;%dH", y, x);
+    return 0;
+}
+
+int main(void)
+{
+    int gameStatus = 0;
+
+    ShowLogo();
+
+    while (isRunning)
+    {
+        switch (gameStatus)
+        {
+        case 0:
+            gameStatus = RenderTitle();
+            break;
+
+        case 1:
+            gameStatus = People();
+            break;
+        case 2:
+            gameStatus = Manual();
+            break;
+        case 3:
+            gameStatus = Gamestart();
+            break;
+        case 4:
+            gameStatus = Gameover();
+            break;
+        }
+
+    }
+
+    system("cls");
+    move_cursor(0, 25);
+
+    return 0;
+}
